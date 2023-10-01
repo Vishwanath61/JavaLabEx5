21c686a8d0adedc4b3070bb9276af22a6ea68c25
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2023-10-01 19:52:02 +0530

    Add files via upload

--- 3. protected properties /protecteddefault/package1/parentclass.java
+++ 3. protected properties /protecteddefault/package1/parentclass.java
@@ -1,1 +1,7 @@
+package protecteddefault.package1;
 
+public class parentclass 
+{
+    protected String protectedProperty = "This is a protected property";
+    String defaultProperty = "This is a default property";
+}
