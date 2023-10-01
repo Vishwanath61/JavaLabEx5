83fe4d14773270f179dd5d21a3032c3cfdc9763d
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2023-10-01 19:53:25 +0530

    Add files via upload

--- 3. protected properties /protecteddefault/package2/childclass.java
+++ 3. protected properties /protecteddefault/package2/childclass.java
@@ -1,1 +1,16 @@
+package protecteddefault.package2;
 
+import protecteddefault.package1.parentclass;
+
+/**
+ *
+ * @author VISHWA
+ */
+public class childclass extends parentclass
+{
+    public void accessProperties() 
+    {
+        System.out.println(protectedProperty);
+        //System.out.println(defaultProperty); // This will cause a compile error
+    }     
+}
